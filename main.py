947861c89e23bff560b4f95c89373f83d3c3d218
ArtDmSav <artdmsav@gmail.com>
2023-07-06 18:15:27 +0300

    dddd

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetHistoryRequest
 
 import city
-import mysql_message_db
 import price
 import w_csv_data
 
@@ -78,7 +77,7 @@ while True:
                 except AttributeError:  # ошибка преобразования к lower
                     continue
                 # Записываем необходимые данные в таблицу SQL
-                mysql_message_db.write(message.date, citys, pric, message.id, message.chat_id)
+                ####            #mysql_message_db.write(message.date, citys, pric, message.id, message.chat_id)
 
                 # Выводим данные о сообщении для ручного чека
                 print("city = ", citys)
@@ -93,5 +92,5 @@ while True:
     # if total_count_limit != 0 and total_messages >= total_count_limit:
     # break
 # Выводим таблицу SQL для ручного чека
-mysql_message_db.table_view()
+#######mysql_message_db.table_view()
 print("Парсинг сообщений группы успешно выполнен.")  # Сообщение об удачном парсинге чата.
