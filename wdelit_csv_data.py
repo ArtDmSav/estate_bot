12c9ee2f87ba66f9bd153308677e7105dea19089
ArtDmSav <artdmsav@gmail.com>
2023-08-02 23:05:02 +0300

    deleted file

--- wdelit_csv_data.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-
-def write_csv(date_msg, city, price, message_id, message_chat_id):
-    with open("data_base.csv", "a", encoding="UTF-8") as f:
-        writer = csv.writer(f, delimiter=",", lineterminator="\n")
-        # writer.writerow(["date_msg", "city", "price", "message_id", "message_chat_id"])
-        writer.writerow([date_msg, city, price, message_id, message_chat_id])
