fb50cbb4087116270d2d5e10ef2b0e5323cd5f92
ArtDmSav <artdmsav@gmail.com>
2023-08-02 22:55:56 +0300

    transfer telethon func to bot_telethon.py add test send msg func like user from user with save last send msg for control

--- main.py
+++ main.py
@@ -1,131 +1,33 @@
-import configparser
-import logging
-import sys
-import time
-
-from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetHistoryRequest
-
-import city_parsing
-import price_parsing
-import sqlite_create_db
-import sqlite_message_db
-
-start_time = time.time()
-# Write logs
-logging.basicConfig(level=logging.DEBUG, filename='main_estate.log',
-                    format='%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) [%(filename)s]',
-                    datefmt='%d/%m/%Y %I:%M:%S', encoding='UTF-8', filemode='w'
-                    )
-logging.warning('warning')
-logging.critical('critical')
-logging.debug('debug')
-logging.info('info')
-logging.error('error')
-
-# Read config file
-config = configparser.ConfigParser()
-config.read("config.ini")
-
-# Assign data from file
-api_id = int(config['Telegram']['api_id'])
-api_hash = config['Telegram']['api_hash']
-phone = config['Telegram']['username']
-
-# Start telegram client by telethon
-client = TelegramClient(phone, api_id, api_hash)
-client.start()
-
-# Set data
-offset_id = 0
-limit = 500
-end_id = 0
-counter = 0
-del_msg_after_day = 30
-clean_counter = 0
-target_group = 't.me/estatecyprus'
-flag_1 = True
-flag_2 = True
+import wdelit_tg_user_communication
+from bot.bot_telethon import parsing_chat
+from database import sqlite_message_db, sqlite_create_db, sqlite_user_request
 
 # Create database and tables
 sqlite_create_db.create_lots()
 sqlite_create_db.create_users()
-last_msg_id = sqlite_message_db.last_msg_id()
+# Delete old messages
+del_msg_after_day = 4
 sqlite_message_db.del_old_msg(del_msg_after_day)
-# sqlite_message_db.del_repeating_msg()
 
+last_msg_id = sqlite_message_db.last_msg_id()
+parsing_chat(last_msg_id)
 
-# Start telethon body
-while True:
-    history = client(GetHistoryRequest(
-        peer=target_group,  # Don't warn ing, it's OK
-        offset_id=offset_id,  # Offset message ID (only messages previous to the given ID will be retrieved).Exclusive
-        offset_date=None,  # (datetime): Offset date (messages previous to this date will be retrieved). Exclusive
-        add_offset=0,  # (int): Additional message offset (all the specified offsets + this offset = older messages)
-        limit=limit,  # (int | None, optional): Number of messages to be retrieved. Due to limitations with the API
-        # retrieving more than 3000 messages will take longer than half a minute (or even more based on
-        # previous calls).
-        max_id=0,
-        min_id=end_id,  # (int): All the messages with a lower (older) ID or equal to this will be excluded.
-        hash=0
-    ))
-
-    if not history.messages:
-        sqlite_message_db.add_msg_end_id()
-        end_time = time.time()
-        total_time = round(end_time - start_time, 3)
-        print('\n\nProgram takes = ', total_time, 'sec')
-        sys.exit()
-    messages = history.messages
-
-    for message in messages:
-        if message.id <= last_msg_id:
-            sqlite_message_db.add_msg_end_id()
-            end_time = time.time()
-            total_time = round(end_time - start_time, 3)
-            print('\n\nProgram takes = ', total_time, 'sec')
-            sys.exit()
-
-        if flag_1:  # save id first message. it's end id for next iteration
-            end_id = message.id
-            flag_1 = False
-            flag_2 = True
-            print('end_id_if = ', end_id)
-        counter += 1
-        if not message.message == '':  # skip message without text (skip photo, video message)
-            clean_counter += 1
-
-            # Print our count message number, and 'clean' message number (only message with text)
-            print('________________________________________________ \n'
-                  'counter = ', counter,
-                  'clean_counter = ', clean_counter
-                  )
-            # Call find price func in message (use low register for all text)
-            str_message = str(message.message).lower()
-            price = price_parsing.f_price(str_message)
-            # price = price_parsing.f_price(message.message.lower())
-            if price == -1:  # If we can't find price, we move to the next message
-                continue
-
-            print("price = ", price, "€")
-            try:
-                city = city_parsing.parse(message.message.lower())
-            except AttributeError:  # Catch transformation to low error (debug)
-                print(AttributeError)
-                continue
-
-            # Add date to database
-            if flag_2:
-                flag_2 = False
-                sqlite_message_db.write_lots(message.date, city, price, message.id, message.chat_id, end_id)
+active_user_list = sqlite_user_request.active_user()
+print(active_user_list)
+last_sent_msg_id = 0
+flag = True
+for row in active_user_list:
+    data = sqlite_user_request.request(row[0], row[1], row[2], row[4], int(row[3]))
+    for line in data:
+        wdelit_tg_user_communication.send_msg_f_user(line[0], line[1], line[2], line[3])
+        if flag:
+            if line[1] != -1:
+                last_sent_msg_id = line[1]
+                flag = False
             else:
-                sqlite_message_db.write_lots(message.date, city, price, message.id, message.chat_id)
+                last_sent_msg_id = line[0]
+                flag = False
+    sqlite_message_db.last_sent_msg_id(last_sent_msg_id, row[3])
 
-            # Print data for manual check (debug)
-            print("city = ", city)
-            print("data time = ", message.date)
-            print(message.id)
-            print(message.message)
-
-# Print table for manual check (debug)
-# sqlite_message_db.table_view_lots()
+# data = sqlite_user_request.request()
+# print(data)
