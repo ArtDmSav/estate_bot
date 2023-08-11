cfd596e0e8dcb913009fadf549df3824c22102b9
ArtDmSav <artdmsav@gmail.com>
2023-08-11 13:49:40 +0300

    fix bag with incorrect last_send_msg_id when we don't have msg for send add time decorator and func total work time program

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import wdelit_tg_user_communication
 from bot.bot_telethon import parsing_chat
-from database import sqlite_message_db, sqlite_create_db, sqlite_user_request
+from database import sqlite_message_db, sqlite_user_request
+from functions.time_count_decorator import full_time
 
 # Create database and tables
-sqlite_create_db.create_lots()
-sqlite_create_db.create_users()
+# sqlite_create_db.create_lots()
+# sqlite_create_db.create_users()
+
 # Delete old messages
-del_msg_after_day = 4
+del_msg_after_day = 30
 sqlite_message_db.del_old_msg(del_msg_after_day)
 
 last_msg_id = sqlite_message_db.last_msg_id()
@@ -20,6 +22,7 @@ for row in active_user_list:
     data = sqlite_user_request.request(row[0], row[1], row[2], row[4], int(row[3]))
     for line in data:
         wdelit_tg_user_communication.send_msg_f_user(line[0], line[1], line[2], line[3])
+        print(line[0], line[1], line[2], line[3])
         if flag:
             if line[1] != -1:
                 last_sent_msg_id = line[1]
@@ -27,7 +30,10 @@ for row in active_user_list:
             else:
                 last_sent_msg_id = line[0]
                 flag = False
-    sqlite_message_db.last_sent_msg_id(last_sent_msg_id, row[3])
 
-# data = sqlite_user_request.request()
-# print(data)
+    if flag:
+        sqlite_message_db.last_sent_msg_id(last_msg_id, row[3])
+    else:
+        sqlite_message_db.last_sent_msg_id(last_sent_msg_id, row[3])
+
+full_time()
