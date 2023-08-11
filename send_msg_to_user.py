6b12ec6ca045009e8965a0d8a744bb3683f2753a
ArtDmSav <artdmsav@gmail.com>
2023-08-11 13:55:26 +0300

    rename file add time decorator add try except for MessageIdInvalidError

--- send_msg_to_user.py
+++ send_msg_to_user.py
@@ -1,8 +1,12 @@
 import configparser
 
+from telethon.errors.rpcerrorlist import MessageIdInvalidError
 from telethon.sync import TelegramClient
 
+from functions.time_count_decorator import time_count
 
+
+@time_count
 def send_msg_f_user(msg_start=345210, msg_end=-1, user=474103257, chanel=-1001261922335):
     # Считываем учетные данные
     config = configparser.ConfigParser()
@@ -14,7 +18,11 @@ def send_msg_f_user(msg_start=345210, msg_end=-1, user=474103257, chanel=-100126
     phone = config['Telegram']['username']
 
     with TelegramClient(phone, int(api_id), api_hash) as client:
-        if msg_end == -1:
-            client.forward_messages(entity=user, messages=msg_start, from_peer=chanel)
-        else:
-            client.forward_messages(entity=user, messages=[_ for _ in range(msg_start, msg_end)], from_peer=chanel)
+        try:
+            if msg_end == -1:
+
+                client.forward_messages(entity=user, messages=msg_start, from_peer=chanel)
+            else:
+                client.forward_messages(entity=user, messages=[_ for _ in range(msg_start, msg_end)], from_peer=chanel)
+        except MessageIdInvalidError:
+            print("telethon.errors.rpcerrorlist.MessageIdInvalidError")
