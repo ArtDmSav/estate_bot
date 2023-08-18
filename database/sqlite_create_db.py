b50c6211337e45617a39fabb5972b461ea8edc41
ArtDmSav <artdmsav@gmail.com>
2023-08-18 11:32:44 +0300

    Add path with pathlib Minor changes

--- database/sqlite_create_db.py
+++ database/sqlite_create_db.py
@@ -1,53 +1,51 @@
+import pathlib
 import sqlite3 as sql
 
 from functions.time_count_decorator import time_count
 
+# Write path to work directory
+dir_path = pathlib.Path.cwd()
+path = pathlib.Path(dir_path, 'database', 'estate.db')
+
 
 @time_count
 def create_lots():
-    connect = sql.connect("database/estate.db")
+    connect = sql.connect(path)
 
     with connect:
-        # получаем количество таблиц с нужным нам именем
         data = connect.execute("select count(*) from sqlite_master where type='table' and name='lots'")
         for row in data:
-            # если таких таблиц нет
             if row[0] == 0:
-                # создаём таблицу для товаров
                 with connect:
                     connect.execute("""
                         CREATE TABLE lots (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
                             city VARCHAR(20),
                             price INTEGER(7),
                             date DATETIME,
-                            message_id INTEGER(7),
+                            message_id INTEGER(7) NOT NULL UNIQUE,
                             message_end_id INTEGER(7),
-                            chat_id BIGINTEGER(14) 
+                            chat_id BIGINTEGER(14),
+                            PRIMARY KEY(message_id)
                         );
                     """)
 
 
 @time_count
 def create_users():
-    connect = sql.connect("database/estate.db")
-
+    connect = sql.connect(path)
     with connect:
-        # получаем количество таблиц с нужным нам именем
         data = connect.execute("select count(*) from sqlite_master where type='table' and name='users'")
         for row in data:
-            # если таких таблиц нет
             if row[0] == 0:
-                # создаём таблицу для товаров
                 with connect:
                     connect.execute("""
                         CREATE TABLE users (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
                             city VARCHAR(20),
                             min_price INTEGER(7),
                             max_price INTEGER(7),
-                            msg_chat_id VARCHAR(20),
+                            msg_chat_id VARCHAR(20) UNIQUE,
                             active INTEGER(1),
-                            last_msg_id INTEGER(7)
+                            last_msg_id INTEGER(7),
+                            PRIMARY KEY(msg_chat_id)
                         );
                     """)
