9213e75b6632590156b7080a64de146b0e0bb057
ArtDmSav <artdmsav@gmail.com>
2023-08-20 00:26:57 +0300

    Minor changes

--- bot/send_msg_to_user.py
+++ bot/send_msg_to_user.py
@@ -1,4 +1,5 @@
 import configparser
+import time
 
 from telethon.errors.rpcerrorlist import MessageIdInvalidError
 from telethon.sync import TelegramClient
@@ -8,7 +9,7 @@ from functions.time_count_decorator import time_count
 
 
 @time_count
-def send_msg_f_user(msg_start=345210, msg_end=-1, user=474103257, chanel=-1001261922335):
+def send_msg_f_user(msg_start, msg_end, user, chanel):
     # Считываем учетные данные
     config = configparser.ConfigParser()
     config.read("config/config.ini")
@@ -52,9 +53,11 @@ def send_msgs_f_users(active_user_list, last_msg_id):
                     if line[1] == -1:
 
                         client.forward_messages(entity=line[2], messages=line[0], from_peer=line[3])
+                        time.sleep(1)
                     else:
                         client.forward_messages(entity=line[2], messages=[_ for _ in range(line[0], line[1])],
                                                 from_peer=line[3])
+                        time.sleep(1)
                 except MessageIdInvalidError:
                     print("telethon.errors.rpcerrorlist.MessageIdInvalidError")
 
@@ -66,6 +69,55 @@ def send_msgs_f_users(active_user_list, last_msg_id):
                     else:
                         last_sent_msg_id = line[0]
                         flag = False
+
+            if flag:
+                sqlite_commit_db.last_sent_msg_id(last_msg_id, row[3])
+            else:
+                sqlite_commit_db.last_sent_msg_id(last_sent_msg_id, row[3])
+
+
+# @time_count
+def send_link_to_bot_t():
+    # Считываем учетные данные
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+
+    # Присваиваем значения внутренним переменным
+    api_id = config['Telegram']['api_id']
+    api_hash = config['Telegram']['api_hash']
+    bot_token = config['Telegram']['bot_token']
+
+    # We have to manually call "start" if we want an explicit bot token
+    bot = TelegramClient('bot', int(api_id), api_hash).start(bot_token=bot_token)
+
+    # But then we can use the client instance as usual
+    with bot:
+        bot.send_message(474103257, 'Thanks for the Telethon library!')
+
+
+def send_link_to_bot(active_user_list, last_msg_id):
+    # Считываем учетные данные
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+
+    # Присваиваем значения внутренним переменным
+    api_id = config['Telegram']['api_id']
+    api_hash = config['Telegram']['api_hash']
+    bot_token = config['Telegram']['bot_token']
+
+    # We have to manually call "start" if we want an explicit bot token
+    bot = TelegramClient('bot', int(api_id), api_hash).start(bot_token=bot_token)
+
+    with bot:
+        # bot.send_message(474103257, 'Thanks for the Telethon library!')
+        for row in active_user_list:
+            data = sqlite_view_db.request(row[0], row[1], row[2], row[4], int(row[3]))
+            flag = True
+            for line in data:
+                text = f"\n\n{line[4]}\nДанное бъявление доступно по ссылке ->>>  {line[3]}/{line[0]} "
+                bot.send_message(line[2], text)
+                last_sent_msg_id = line[0]
+                flag = False
 
             if flag:
                 sqlite_commit_db.last_sent_msg_id(last_msg_id, row[3])
