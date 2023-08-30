727e959faa1b69859c977da3dee0f5b1fc0d1ee8
ArtDmSav <artdmsav@gmail.com>
2023-08-30 16:48:48 +0300

    Minor changes

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import time
-
-from schedule import every, repeat, run_pending
-
 from bot.bot_telethon import parsing_chat
 from bot.send_msg_to_user import send_link_to_bot
 from database import sqlite_commit_db, sqlite_view_db
+from functions.time_count_decorator import full_time
 
 
-@repeat(every(5).minutes)
+# @repeat(every(9).minutes)
 def do_it():
     # Create database and tables
     # sqlite_create_db.create_lots()
     # sqlite_create_db.create_users()
 
     # Delete old messages
-    del_msg_after_day = 30
+    del_msg_after_day = 5
     sqlite_commit_db.del_old_msg(del_msg_after_day)
 
     last_msg_id = sqlite_view_db.last_msg_id()
@@ -22,9 +19,10 @@ def do_it():
 
     active_user_list = sqlite_view_db.active_user()
     send_link_to_bot(active_user_list, last_msg_id)
-    # full_time()
+    full_time()
 
 
-while True:
-    run_pending()
-    time.sleep(5)
+do_it()
+# while True:
+#     run_pending()
+#     time.sleep(5)
