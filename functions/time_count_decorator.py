6ecd595de3f46d614cd41ffb87a8de8c4ca1456c
ArtDmSav <artdmsav@gmail.com>
2023-08-30 16:52:45 +0300

    Add cleaner for time list

--- functions/time_count_decorator.py
+++ functions/time_count_decorator.py
@@ -7,8 +7,10 @@ def full_time():
     s_time = round(sum(f_time), 2)
     if s_time > 60:
         print(f"[*] Время выполнения всей программы {int(s_time // 60)} минут {int(s_time % 60)} секунд.")
+        f_time.clear()
     else:
         print(f"[*] Время выполнения всей программы {s_time} секунд.")
+        f_time.clear()
 
 
 def time_count(func):
