296ea14708991bd72253e7ffc82eb50f23138d57
ArtDmSav <artdmsav@gmail.com>
2023-09-21 10:12:22 +0300

    Refactoring

--- estate_bot/main.py
+++ estate_bot/main.py
@@ -1,28 +1,22 @@
 from estate_bot.bot.bot_telethon import parsing_chat
 from estate_bot.bot.send_msg_to_user import send_link_to_bot
-from estate_bot.database import sqlite_commit_db, sqlite_view_db
+from estate_bot.config.data import DEL_MSG_AFTER_DAY
+from estate_bot.database.sqlite_commit_db import del_old_msg
+from estate_bot.database.sqlite_view_db import last_msg_id
 from estate_bot.functions.time_count_decorator import full_time
 
 
-# @repeat(every(9).minutes)
 def do_it():
     # Create database and tables
     # sqlite_create_db.create_lots()
     # sqlite_create_db.create_users()
 
-    # Delete old messages
-    del_msg_after_day = 7
-    sqlite_commit_db.del_old_msg(del_msg_after_day)
+    lst_msg_id = last_msg_id()
 
-    last_msg_id = sqlite_view_db.last_msg_id()
-    parsing_chat(last_msg_id)
-
-    active_user_list = sqlite_view_db.active_user()
-    send_link_to_bot(active_user_list, last_msg_id)
+    del_old_msg(DEL_MSG_AFTER_DAY)
+    parsing_chat(lst_msg_id)
+    send_link_to_bot(lst_msg_id)
     full_time()
 
 
 do_it()
-# while True:
-#     run_pending()
-#     time.sleep(5)
